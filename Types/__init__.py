a0d62efb9707a2f50f82a2b8b0aa23780db0ebc3
CGHoussem <cjhoussem@gmail.com>
2021-02-04 11:42:49 +0100

    major update

--- Types/__init__.py
+++ Types/__init__.py
@@ -1,7 +1,99 @@
-# This class makes the directory "types" a python package
+# This class is a python package
 
-from .BaseStation import BaseStation
-from .Machine import Machine
-from .Packet import Packet
-from .Slot import Slot
-from .Trame import Trame
+import random, string
+from enum import Enum
+
+MAX_NUM_OF_SLOTS = 10
+
+class Ack(Enum):
+    RECEIVED = 0.9
+    NOT_SINGLE = 0.4
+    NOT_RECEIVED = 0.1
+
+
+class Packet(object):
+    """
+    Packet class
+    """
+    def __init__(self, value=None) -> None:
+        super().__init__()
+        
+        # Attributing a random caracter as a value for the packet
+        self.value = ''.join(random.choice(string.ascii_letters))
+        # If the value is passed in the parameters, attribute that instead
+        if value:
+            self.value = value
+   
+    def __repr__(self) -> str:
+        return self.value
+
+
+class Frame(object):
+    """
+    Frame class (c'est la Trame en français)
+    """
+    COUNT = 0 # static number defining the total number of initiated machines 
+    
+    def __init__(self):
+        self.index = Frame.COUNT
+        # create a list of empty slots
+        self.slots = [[] for _ in range(MAX_NUM_OF_SLOTS)]
+
+        Frame.COUNT += 1
+    
+    def receive_packet(self, packet:Packet):
+        """
+        This functions assings a reveived packet to a slot.
+        The slot will be chosen randomly.
+        """
+        index = random.randint(0, 9)
+        self.slots[index].append(packet)
+
+    def __repr__(self) -> str:
+        r = f'Frame {self.index} | \t'
+        for slot in self.slots:
+            r += ''.join(str(slot))
+        r += '\n'
+        return r
+
+
+class BaseStation(object):
+    """
+    Base Station
+    """
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    ## TODO
+
+
+class Equipment(object):
+    """
+    Equipment class
+    """
+    COUNT = 0 # static number defining the total number of initiated machines 
+    
+    def __init__(self, packets_count=None, distribution_times=None):
+        self.index = Equipment.COUNT
+
+        self.distribution_times = distribution_times
+
+        # Create a list of packets
+        self.packets = [Packet() for _ in range(packets_count)]
+
+        Equipment.COUNT += 1
+
+    def send_packets(self, frame:Frame):
+        for packet in self.packets:
+            self.__send_packet(packet, frame)
+
+    def __send_packet(self, packet:Packet, frame:Frame):
+        # we choose the number of copies (k)
+        k = random.randint(1, 3)
+        for _ in range(k):
+            frame.receive_packet(packet)
+        
+
+    def __repr__(self) -> str:
+        return f"Packets\t>\tDitribution Times\n{str(self.packets)}\t>\t{str(self.distribution_times)}\n"
--- main.py
+++ main.py
@@ -6,12 +6,116 @@
 """
 
 from Types import BaseStation
-from Types import Machine
+from Types import Equipment
 from Types import Packet
-from Types import Slot
-from Types import Trame
+from Types import Frame
 
+import random
+import math
+
+def get_distrubtion_times(lmbd:float):
+    times = []
+    
+    old_t = 0
+    for i in range(packets_count):
+        r = random.uniform(0,1)
+        t = round(-math.log(r) / lmbd)
+        times.append(old_t + t)
+        old_t += t
+
+    return times
 
 if __name__ == "__main__":
-    ## Simulation..
-    pass
+    random.seed()
+
+    # Input for equipment count
+    equipments_count = -1
+    while equipments_count < 10 or equipments_count > 20:
+        try:
+            equipments_count = input("Enter how many equipements are there (default = 10): ")
+            if not equipments_count:
+                equipments_count = 10
+            else:
+                equipments_count = int(equipments_count)
+        except ValueError:
+            print("Error: must be an integer! (10 <= number <= 20)")
+        if equipments_count < 10 or equipments_count > 20:
+            print("Warning: must be a number between 10 and 20")
+
+    # Input for how many packets will be sent by equipment
+    packets_count = -1
+    while packets_count < 0:
+        try:
+            packets_count = int(input("Enter the number of packets each equipment will send: "))
+        except ValueError:
+            print("Error: must be an integer! (> 0)")
+    
+    # Input for equipment count
+    lmbd = -1
+    while lmbd < 0 or lmbd > 2:
+        try:
+            lmbd = input("Enter the lambda value (default = 0.5): ")
+            if not lmbd:
+                lmbd = 0.5
+            else:
+                lmbd = float(lmbd)
+        except ValueError:
+            print("Error: must be a float! (0 < lambda < 2)")
+        if lmbd < 0 or lmbd > 2:
+            print("Warning: must be a number between 0 and 2")
+
+    # Create the Base Station
+    bs = BaseStation()
+    
+    # Create a list of equipments
+    equipments = []
+    for _ in range(equipments_count):
+        e = Equipment(packets_count)
+        e.distribution_times = get_distrubtion_times(lmbd)
+        equipments.append(e)
+    
+    print(equipments[0])
+
+    # Create a list of frames
+    frames = [Frame() for _ in range(equipments_count)]
+
+    for i, e in enumerate(equipments):
+        e.send_packets(frames[i])
+    
+    print(frames[0])
+
+    ## GENERAL INFORMATIONS
+    # the packets arrives following Poisson using parameter LAMBDA
+    # the equipment chooses randomly k SLOTs from the 10 SLOTs
+    #   k : random between 1 and 3
+    
+    ## GUIDELINE
+    # 1) equipment sends packets to BS
+    # sends multiple copies of it's packet in a frame that has 10 SLOTs
+    # 2) a collision is when two equipments or more sends their packet to the same SLOT
+    # 3) if a packet didn't collide with another packet
+    # BS eliminates this copie from frame
+    # T
+
+    """ EXAMPLE
+        D'aprés les videos en ligne (https://youtu.be/RN4XKqTEjDA)
+
+        S represents a slot
+        X represents a frame (trame en français :p)
+        C represents a collision
+
+        |------------|S (1)|S (2)|S (3)|S (4)|S (5)|S (6)|S (7)|S (8)|S (9)|S(10)|
+        |EQUIPMENT 1 |--X--|-----|--X--|-----|--X--|-----|-----|--X--|-----|--X--| <- Frame 1
+        |EQUIPMENT 2 |--X--|-----|-----|--X--|-----|-----|-----|-----|--X--|-----| <- Frame 2
+        |EQUIPMENT 3 |-----|--X--|-----|-----|--X--|-----|--X--|-----|--X--|-----| <- Frame 3
+        |------------|-----------------------------------------------------------|
+        |BROADCAST   |--C--|--X--|--X--|--X--|--C--|-----|--X--|--X--|--C--|--X--| <- Frame of BS ?
+        |------------|-----------------------------------------------------------|
+
+
+        D'aprés ceci, je sais pas que ce représente les packets dans le sujet? 
+            Est ce que ils sont les trames dans la vidéo? 
+                ou 
+            Ils sont des "data" dans les trames dans la vidéo?
+        
+    """
